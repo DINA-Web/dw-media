5e927a826391fe48a5f3c271ae94c64313a3f916
Ingimar Erlingsson <inkimar@gmail.com>
2017-12-08 14:04:54 +0100

    Change , able to run a test from outside

--- self-test/db_check.sh
+++ self-test/db_check.sh
@@ -1,1 +1,1 @@
-docker exec -it mediadocker_dbmedia_1 mysql -u mediaserver -pmediaserver -e "select * from nf_media.ADMIN_CONFIG"
+docker exec -it mediadocker_db.media_1  mysql -u mediaserver -pmediaserver -e "select * from nf_media.ADMIN_CONFIG"
